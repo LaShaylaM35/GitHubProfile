fbb135d97f87437d23f8a74e037b5da11df65739
LaShaylaM35 <lashaylam@yahoo.com>
2025-12-19 17:09:54 -0600

    Added an ordered list to Javascript file

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ async function getUser() {
   console.log(data);
 
   let userData = document.getElementById("user-data");
-  userData.innerHTML = `User Name: ${data.login} Bio: ${data.bio}  Public Repos: ${data.public_repos} <br>`;
+  userData.innerHTML = `<ol><li>User Name: ${data.login}</li><li>Bio: ${data.bio}</li><li>Public Repos: ${data.public_repos}</li></ol>`;
 
   userData.innerHTML += `<img src="${data.avatar_url}" alt="User Avatar">`;
 }
